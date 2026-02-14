992b07e7574c84722600c0fc53977e95f9a99653
kunlilin2991 <kunlilin2991@163.com>
2026-02-14 11:29:17 +0800

    Create lazyvim_vscode_options.lua with settings
    Add configuration options for LazyVim in Lua.

---
+++ lazyvim_vscode_options.lua
@@ -0,0 +1,60 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- leader key
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+
+--le lb in keymap.lua
+
+-- 搜索相关设置
+vim.opt.hlsearch = true       -- 高亮搜索
+vim.opt.incsearch = true      -- 实时搜索
+vim.opt.ignorecase = true     -- 忽略大小写
+vim.opt.smartcase = true      -- 有大写时区分大小写
+
+-- 语法与显示
+vim.cmd("syntax on")          -- 语法高亮
+vim.opt.showmatch = true      -- 匹配符号高亮
+-- 文件编码
+vim.opt.encoding = "utf-8"
+-- 使用 Unix/Linux 换行符（LF）
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = { "unix", "dos", "mac" }
+
+-- 缩进
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- 高亮当前行跟列
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+-- 只在当前窗口高亮（避免分屏干扰）
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  callback = function()
+    vim.opt_local.cursorline = true
+    vim.opt_local.cursorcolumn = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  callback = function()
+    vim.opt_local.cursorline = false
+    vim.opt_local.cursorcolumn = false
+  end,
+})
+-- Insert 模式只高亮行
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    vim.opt_local.cursorcolumn = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    vim.opt_local.cursorcolumn = true
+  end,
+})
+-- 确保neovim不使用系统剪贴板，除非用户明确设置
+vim.opt.clipboard = ""
