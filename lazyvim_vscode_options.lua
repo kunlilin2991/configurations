cc65967e43dd71602579e0789eb7be38e1511a41
kunlilin2991 <kunlilin2991@163.com>
2026-02-24 18:13:35 +0800

    Configure indentation and disable global diagnostics

--- lazyvim_vscode_options.lua
+++ lazyvim_vscode_options.lua
@@ -58,3 +58,22 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 -- 确保neovim不使用系统剪贴板，除非用户明确设置
 vim.opt.clipboard = ""
+
+-- 关闭全局诊断，用户可以根据需要在特定文件或项目中启用
+vim.diagnostic.enable(false)
+
+-- 默认缩进为 4 个空格
+vim.opt.tabstop = 4        -- 一个 Tab 显示为 4 个空格
+vim.opt.shiftwidth = 4    -- 自动缩进使用 4 个空格
+vim.opt.softtabstop = 4   -- 插入模式下 Tab = 4 个空格
+vim.opt.expandtab = true  -- 使用空格代替 Tab
+-- linux kernel风格是8个
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.softtabstop = 8
+    vim.opt_local.expandtab = false
+  end,
+})
